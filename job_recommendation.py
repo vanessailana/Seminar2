5212919b2087f6dafaa4fe82f93a9fe86cbafba1
vanessa <vanessa@vanessas-mbp.lan>
2019-04-28 21:37:38 -0700

    test

--- job_recommendation.py
+++ job_recommendation.py
@@ -19,13 +19,17 @@ from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from time import mktime
 from datetime import datetime
 import pymysql
+from flask_cors import CORS
 
-import requests
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app = Flask(__name__)
 
 def connect_db():
-    return pymysql.connect(host='35.238.104.188', port=3306, user='root', password='root', db='680', cursorclass=pymysql.cursors.DictCursor)
+    return pymysql.connect(host='35.238.104.188', user='root', password='root', db='680', port=3306,autocommit = True, charset = 'utf8mb4',
+                           cursorclass = pymysql.cursors.DictCursor)
 def get_db():
     '''Opens a new database connection per request.'''
     if not hasattr(g, 'db'):
@@ -34,22 +38,69 @@ def get_db():
 
 
 
-@app.route('/recommendations')
-def jobRec():
+@app.route('/recommendations/<int:user_id>')
+def jobRec(user_id):
    
   
-  con= pymysql.connect(host='35.238.104.188', port=3306, user='root', password='root', db='680', cursorclass=pymysql.cursors.DictCursor)
+   cursor=get_db().cursor();
+
+   cursor().execute("SELECT * FROM jobs")
+
+
+    #jobs users can get recommendatiosn
+   jobs = pd.DataFrame(cursor.fetchall());
+
+
+   cursor().execute("SELECT *from  users ");
+
+   user_based_approach = pd.DataFrame(cursor.fetchall());
+
+   cursor().execute("SELECT * from  applicants");
+   apps = pd.DataFrame(cursor.fetchall());
+
+   user_based_approach[12] = user_based_approach[12].fillna('')
+   user_based_approach[13] = user_based_approach[13].fillna('')
+   user_based_approach[16] = str(user_based_approach[16].fillna(''))
+   user_based_approach[7] = user_based_approach[12] + user_based_approach[13] + user_based_approach[16]
+
+   tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+   tfidf_matrix = tf.fit_transform(user_based_approach[7])
+
+   cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+   indices = pd.Series(user_based_approach.index, index=user_based_approach[0])
+    
+   idx=indices[user_id];
    
-       
-  cur = con.cursor()
-  cur.execute("SELECT * FROM users")
-       
-  rows = cur.fetchall()
+   sim_scores = list(enumerate(cosine_sim[idx]))
+    #print (sim_scores)
+   sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+   user_indices = [i[0] for i in sim_scores]
+
+   user_based_approach = user_based_approach.reset_index()
+
+
+   indices = pd.Series(user_based_approach.index, index=user_based_approach[0])
 
    
-   
-   
-  return jsonify(rows)
+
+   sim_scores = list(enumerate(cosine_sim[idx]))
+        #print (sim_scores)
+   sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+   user_indices = [i[0] for i in sim_scores]
+
+   jobs_userwise = apps[4].isin(user_indices) #
+   df1 = pd.DataFrame(data = apps[jobs_userwise])
+   joblist = df1[3].tolist()
+   Job_list = jobs[0].isin(joblist)
+
+    
+    #job recommendations 
+   df_temp = pd.DataFrame(data = jobs[Job_list])
+
+   stuff=df_temp.to_json(orient='records')
+    
+
+   return str(jobs);
 
 
 @app.route('/stackrec')
@@ -66,5 +117,6 @@ def recStack():
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 
