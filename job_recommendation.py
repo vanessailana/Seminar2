ee761ea76da67a9ceb39927d638bfe8b1a6bfb01
vanessa <vanessa@vanessas-mbp.lan>
2019-04-30 19:01:52 -0700

    changed it to before

--- job_recommendation.py
+++ job_recommendation.py
@@ -47,9 +47,57 @@ def jobRec(user_id):
     jobs = pd.DataFrame(mycursor.fetchall());
 
 
+    mycursor.execute("SELECT *from  users ");
+
+    user_based_approach = pd.DataFrame(mycursor.fetchall());
+
+    mycursor.execute("SELECT * from  applicants");
+    apps = pd.DataFrame(mycursor.fetchall());
+
+    user_based_approach[12] = user_based_approach[12].fillna('') 
+    user_based_approach[13] = user_based_approach[13].fillna('')
+    user_based_approach[16] = str(user_based_approach[16].fillna(''))
+    user_based_approach[7] = user_based_approach[12] + user_based_approach[13] + user_based_approach[16]
+
+    tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(user_based_approach[7])
+
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    indices = pd.Series(user_based_approach.index, index=user_based_approach[0])
+    
+    idx=indices[user_id];
+   
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    #print (sim_scores)
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    user_indices = [i[0] for i in sim_scores]
+
+    user_based_approach = user_based_approach.reset_index()
+
+
+    indices = pd.Series(user_based_approach.index, index=user_based_approach[0])
+
    
 
-    return str(jobs);
+    sim_scores = list(enumerate(cosine_sim[idx]))
+        #print (sim_scores)
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    user_indices = [i[0] for i in sim_scores]
+
+    jobs_userwise = apps[4].isin(user_indices) #
+    df1 = pd.DataFrame(data = apps[jobs_userwise])
+    joblist = df1[3].tolist()
+    Job_list = jobs[0].isin(joblist)
+
+    
+    #job recommendations 
+    df_temp = pd.DataFrame(data = jobs[Job_list])
+
+    stuff=df_temp.to_json(orient='records')
+    
+
+    return stuff;
+
 
 @app.route('/stackrec')
 @cross_origin(origin="http://localhost:4200/unique_recommendations")
