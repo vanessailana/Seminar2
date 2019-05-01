749b6c76549b8b3db677dddd753f47954e217eaa
vanessa <vanessa@vanessas-mbp.lan>
2019-04-30 19:51:21 -0700

    job rec stuff

--- job_recommendation.py
+++ job_recommendation.py
@@ -54,7 +54,49 @@ def jobRec(user_id):
     mycursor.execute("SELECT * from  applicants");
     apps = pd.DataFrame(mycursor.fetchall());
 
-    return str(apps);
+    user_based_approach[12] = user_based_approach[12].fillna('test')
+    user_based_approach[13] = user_based_approach[13].fillna('test')
+    user_based_approach[16] = str(user_based_approach[16].fillna('test'))
+    user_based_approach[7] = user_based_approach[12] + user_based_approach[13] + user_based_approach[16]
+
+    tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(user_based_approach[7])
+
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    indices = pd.Series(user_based_approach.index, index=user_based_approach[0])
+    
+    idx=indices[user_id];
+   
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    #print (sim_scores)
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    user_indices = [i[0] for i in sim_scores]
+
+    user_based_approach = user_based_approach.reset_index()
+
+
+    indices = pd.Series(user_based_approach.index, index=user_based_approach[0])
+
+   
+
+    sim_scores = list(enumerate(cosine_sim[idx]))
+        #print (sim_scores)
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    user_indices = [i[0] for i in sim_scores]
+
+    jobs_userwise = apps[4].isin(user_indices) #
+    df1 = pd.DataFrame(data = apps[jobs_userwise])
+    joblist = df1[3].tolist()
+    Job_list = jobs[0].isin(joblist)
+
+    
+    #job recommendations 
+    df_temp = pd.DataFrame(data = jobs[Job_list])
+
+    stuff=df_temp.to_json(orient='records')
+    
+
+    return stuff;
 
 
 @app.route('/stackrec')
