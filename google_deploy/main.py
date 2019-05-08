434a101f82ff8bf3d9e2c91a6571d4dd167cc5cb
vanessa <vanessa@vanessas-mbp.lan>
2019-05-08 00:02:38 -0700

    model for deployment

--- google_deploy/main.py
+++ google_deploy/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random
 import re, cgi
+from flask import Flask,jsonify, render_template, request
 from sklearn.utils import shuffle
 import ast 
 import random
@@ -20,14 +21,15 @@ from time import mktime
 from datetime import datetime
 import pymysql
 
-import atoma
 import requests
 
+# [START create_app]
+app = Flask(__name__)
+# [END create_app]
 
 
-
-#@app.route('/recommendations/<int:user_id>')
-def jobRec():
+@app.route('/recommendations/<int:userid>')
+def jobRec(userid):
    
 
     mydb = mysql.connector.connect(host="35.238.104.188",user="root",passwd="root",port=3306, db='680'
@@ -38,7 +40,7 @@ def jobRec():
     # get jobs 
 
     mycursor.execute("SELECT * FROM jobs")
-
+    
 
     #jobs users can get recommendatiosn
     jobs = pd.DataFrame(mycursor.fetchall());
@@ -46,7 +48,10 @@ def jobRec():
 
     mycursor.execute("SELECT *from  users_ml ");
 
+
+
     user_based_approach = pd.DataFrame(mycursor.fetchall());
+
 
     mycursor.execute("SELECT * from  applicants");
     apps = pd.DataFrame(mycursor.fetchall());
@@ -70,96 +75,67 @@ def jobRec():
 
     indices = pd.Series(jobs.index,index=jobs['Title'])
     
-    title='Maintenance Tech';
-    
-    if title not in indices:
-        a=recStack();
-        print(a);
-    else:
-
-        idx = indices[title]
+    user_based_approach[7]=user_based_approach[7].fillna('');
         
-        sim_scores = list(enumerate(cosine_sim[idx]))
+    user_based_approach[8]=user_based_approach[8].fillna('');
         
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        
-        job_indices = [i[0] for i in sim_scores]
-        
-        print("Similar jobs based on the job you picked\n")
-        
-        print(jobs[7].iloc[job_indices].head(10))
-        
-        user_based_approach[7]=user_based_approach[7].fillna('');
-        
-        user_based_approach[8]=user_based_approach[8].fillna('');
-        
-        user_based_approach[11]=str(user_based_approach[11].fillna(''));
+    user_based_approach[11]=str(user_based_approach[11].fillna(''));
      
-        user_based_approach[7]=user_based_approach[8]+ user_based_approach[11] +  user_based_approach[7];
+    user_based_approach[7]=user_based_approach[8]+ user_based_approach[11] +  user_based_approach[7];
         
-        tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
+    tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
         
-        tfidf_matrix = tf.fit_transform(user_based_approach[7])
+    tfidf_matrix = tf.fit_transform(user_based_approach[7])
 
-        cosine_sim = linear_kernel(tfidf_matrix)
+    cosine_sim = linear_kernel(tfidf_matrix)
         
-        user_based_approach = user_based_approach.reset_index()
+    user_based_approach = user_based_approach.reset_index()
         
-        userid = user_based_approach[0]
+    userid = user_based_approach[0]
         
-        indices = pd.Series(user_based_approach.index, index=user_based_approach[4])
+    indices = pd.Series(user_based_approach.index, index=user_based_approach[4])
         
         #user_id, replace but eventnually needs to be a parameter for function
-        idx = indices[1]
+    idx = indices[1]
 
-        sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = list(enumerate(cosine_sim[idx]))
 
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-        user_indices = [i[0] for i in sim_scores]
+    user_indices = [i[0] for i in sim_scores]
         
-        jobs_userwise = apps[4].isin(user_indices) #
+    jobs_userwise = apps[4].isin(user_indices) #
         
-        df1 = pd.DataFrame(data = apps[3])
+    df1 = pd.DataFrame(data = apps[3])
         
-        joblist = df1[3].tolist()
+    joblist = df1[3].tolist()
 
-        check_user=user_based_approach[user_based_approach.index == 1]
+    check_user=user_based_approach[user_based_approach.index == userid]
 
-        Job_list = jobs[0].isin(joblist)
+    Job_list = jobs[0].isin(joblist)
         
-        results=pd.DataFrame(data=jobs[Job_list])
-        
-        
-        
-        
-        
-        if len(check_user[3]) >0 and len(results)==0:
-        
-        
-        
+    results=pd.DataFrame(data=jobs[Job_list])
+    if len(check_user[3]) >0 or len(results)==0:
             feed=feedparser.parse("http://careers.stackoverflow.com/jobs/feed?location="+check_user[3])
             entry=sorted(feed.entries[:50], key=lambda x: random.random())
             stackOverflowJobs = []
             for e in entry:
                 job = {'title': e.title, 'desc': e.description, 'location': e.location}
                 stackOverflowJobs.append(job)
-                print(stackOverflowJobs)
+                print(jsonify(stackOverflowJobs))
 
-
-        if len(results)==0 and len(check_user[3])==0:
-                recStack()
-                    
-        else:
+    else:
+        print("recommendations based on your profile")
+        results.sample(frac=1).head(20)
+        results.to_json(orient='records');
         
-            print("recommendations based on your profile")
-            print(results.sample(frac=1).head(20))
 
 
 
-        return;
+    return jsonify(str(results))
+@app.route('/recStack')
 def recStack():
-    randomCity=['losangeles','sanfransico','newyork','miami','london','washington']
+    randomCity=['losangeles','sanfransisco','newyork','miami','london','washington']
     randomElement=random.choice(randomCity)
     feed=feedparser.parse("http://careers.stackoverflow.com/jobs/feed?location="+randomElement)
     entry=sorted(feed.entries[:50], key=lambda x: random.random())
@@ -167,13 +143,10 @@ def recStack():
     for e in entry:
         job = {'title': e.title, 'desc': e.description, 'location': e.location}
         stackOverflowJobs.append(job)
-        return stackOverflowJobs;
+        return jsonify(stackOverflowJobs);
 
 
-def shuffle(df, n=1, axis=0):
-    df = df.copy()
-    for _ in range(n):
-            df.apply(np.random.shuffle, axis=axis)
-            return df
 
-print(jobRec())
+
+
+app.run();
